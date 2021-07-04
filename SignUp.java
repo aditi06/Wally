78dc545af73f7a04430b2903500d142ea5649c75
aditi06 <86134712+aditi06@users.noreply.github.com>
2021-07-03 18:08:26 -0700

    Create SignUp.java

---
+++ SignUp.java
@@ -0,0 +1,51 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.content.SharedPreferences;
+import android.content.Context;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText pwdconfirm;
+    private EditText enterName;
+    private final String CHOICE = "our_shared_pref";
+    private Button registerAccount;
+    private EditText pwd;
+
+    @Override
+    protected void onCreate(Bundle instance) {
+        super.onCreate(instance);
+        setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().setTitle("Wally");
+        pwdconfirm = findViewById(R.id.passwordConfirmation);
+        registerAccount = findViewById(R.id.makeAccount);
+        enterName = findViewById(R.id.nameofuser);
+        pwd = findViewById(R.id.password);
+
+        registerAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View c) {
+                String name = enterName.getText().toString();
+                String password = pwd.getText().toString();
+                String pwdConfirm = pwdconfirm.getText().toString();
+
+            if(password.equals(pwdConfirm)){
+                SharedPreferences credentials = getSharedPreferences(CHOICE, Context.MODE_PRIVATE);
+                SharedPreferences.Editor change;
+                change = credentials.edit();
+                change.putString("Username", name);
+                change.putString("Password", password);
+                change.commit();
+                SignUpActivity.this.finish();
+
+            }
+            }
+        });
+
+    }
+}
